309d3eaa5d6069b23d6c0a9182c4608457ebd1b5
Maciazxp <santiago.macias.fernandez@gmail.com>
2026-08-31 23:10:32 -0600

    added the detection animation to enemie
    the enemies now have an animation when the player enter on his area. then they start to follow the player when the animation is over

--- scenes/enemigos/enemigo_1.gd
+++ scenes/enemigos/enemigo_1.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var area_2d : Area2D
 @export var min_speed: float = 100.0
 @export var max_speed: float = 400.0
+@export var animacion: AnimatedSprite2D
+
 
 var speed: float
 
@@ -21,6 +23,7 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	# Solo se mueve si esta en modo persecución y esta el jugador localizado
 	if persiguiendo and jugador != null:
+		animacion.play("idle")
 		# direction_to() calcula automáticamente un vector normalizado hacia el objetivo
 		var direction = global_position.direction_to(jugador.global_position)
 		velocity = direction * speed
@@ -40,6 +43,12 @@ func _on_activate_body_entered(body: Node2D) -> void:
 	if body.is_in_group("personajes"):
 		# Guardamos la referencia del personaje que acaba de entrar al área
 		jugador = body
+		animacion.play("detection")
+		var timer : Timer = Timer.new() #variable timer de tipo Timer que le asignamos un contador con Timer.new()
+	#esto para que la emision de la señal NO SEA INMEDIATA, y podamos ver el color rojo
+		add_child(timer)
+		timer.start(0.6)
+		await timer.timeout
 		persiguiendo = true
 		jugador.personaje_muerto.connect(_on_jugador_muerto)
 		
